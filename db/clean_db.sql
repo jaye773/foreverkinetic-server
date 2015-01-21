4c37905f288d33e11fa41fc4b2312552f7425240
Jaye <jfranklin773@gmail.com>
2015-01-20 23:59:54 -0800

    update sql tables

--- db/clean_db.sql
+++ db/clean_db.sql
@@ -1,6 +1,8 @@
 -- drop tables
 DROP TABLE IF EXISTS media;
-DROP TABLE IF EXISTS exercise_muscle_groups;
+DROP TABLE IF EXISTS muscle_groups_exercise;
+DROP TABLE IF EXISTS exercise_types_exercise;
+DROP TABLE IF EXISTS equipment_exercise;
 DROP TABLE IF EXISTS muscle_groups;
 DROP TABLE IF EXISTS exercise_types;
 DROP TABLE IF EXISTS steps;
